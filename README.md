382e212f4e1fb465070b4da77be9a4516be0bbd3
Tristan Marsh <tristanmarsh@live.com>
2026-01-20 00:43:47 +1100

    report token usage metadata

--- README.md
+++ README.md
@@ -11,5 +11,19 @@ A shell tool to enable agentic flows with remote LLM APIs while executing tools 
 
 ## TODO
 
-- [] Config Python Formatter
-- [] Config Python Type Checker
+- [ ] Config Python Formatter
+- [ ] Config Python Type Checker
+- [ ] Config Logging
+
+## Links
+
+- https://models.dev/
+- https://aistudio.google.com
+
+## Example Google genai client model generate_content result
+
+[GenerateContentResponse](https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentResponse)
+
+```py
+candidates=[Candidate(content=Content(parts=[Part(video_metadata=None, thought=None, code_execution_result=None, executable_code=None, file_data=None, function_call=None, function_response=None, inline_data=None, text='4')], role='model'), citation_metadata=None, finish_message=None, token_count=None, finish_reason=<FinishReason.STOP: 'STOP'>, avg_logprobs=None, grounding_metadata=None, index=0, logprobs_result=None, safety_ratings=None)] create_time=None response_id=None model_version='gemini-2.5-flash' prompt_feedback=None usage_metadata=GenerateContentResponseUsageMetadata(cache_tokens_details=None, cached_content_token_count=None, candidates_token_count=1, candidates_tokens_details=None, prompt_token_count=10, prompt_tokens_details=[ModalityTokenCount(modality=<MediaModality.TEXT: 'TEXT'>, token_count=10)], thoughts_token_count=34, tool_use_prompt_token_count=None, tool_use_prompt_tokens_details=None, total_token_count=45, traffic_type=None) automatic_function_calling_history=[] parsed=None
+```
--- main.py
+++ main.py
@@ -1,23 +1,82 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from typing import Tuple
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
 def main():
+    real = False
+    if len(sys.argv) > 1:
+        print(sys.argv[1])
+        if "real" in sys.argv[1]:
+            real = True
+
     print("Hello from jarvis!")
 
+    get_llm_result(real)
+
+
+def get_llm_client():
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY not found in .env")
 
-    client = genai.Client(api_key=api_key)
-    result = client.models.generate_content(
-        model="gemini-2.5-flash", contents="Why is the sky blue?"
-    )
+    return genai.Client(api_key=api_key)
 
-    print(result.text)
+
+def get_llm_result(real=False):
+    user_prompt = "2+2=?\nNumerical response only."
+    if real:
+        client = get_llm_client()
+        result = call_model(client, user_prompt)
+    else:
+        result = mock_call_model(user_prompt)
+    print_llm_result(user_prompt, *result)
+
+
+def mock_call_model(user_prompt: str) -> Tuple[int, int, str]:
+    print("Log: mock_call_model")
+    return (10, 1, f"{user_prompt}? Wow! That's crazy!")
+
+
+def call_model(client, user_prompt: str) -> Tuple[int, int, str]:
+    try:
+        result = client.models.generate_content(
+            model="gemini-2.5-flash", contents=user_prompt
+        )
+
+        if result is None or result.usage_metadata is None:
+            raise RuntimeError("Usage metadata missing from LLM response", result)
+
+        um = result.usage_metadata
+
+        if um.prompt_token_count is None or um.candidates_token_count is None:
+            raise RuntimeError("Token counts missing from usage_metadata", result)
+
+        if result.text is None:
+            raise Exception("text missing from LLM response", result)
+
+        return (um.prompt_token_count, um.candidates_token_count, result.text)
+    except Exception as e:
+        raise RuntimeError("Unknown error calling model", e)
+
+
+def print_llm_result(
+    user_prompt: str,
+    prompt_tokens: int,
+    response_tokens: int,
+    response: str,
+):
+    template = f"""User prompt: {user_prompt}
+Prompt tokens: {prompt_tokens}
+Response tokens: {response_tokens}
+Response:
+{response}
+"""
+    print(template)
 
 
 if __name__ == "__main__":
